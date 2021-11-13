ecbb0edb6c25c5868aabd078ef48dc9382cdbff9
Ryan Lee <ryanyinlee@protonmail.com>
2021-11-12 17:59:46 -0800

    Final edits.

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 Worked with Jordan Watts and Brian Thornburg.
 
 Code snippet for oldObjects from Michael Metcalf.
+
+11/12/21: 
+
+This program is designed to be a survey of potential products for the Bus Mall catalog.
+
+You run the browser, vote which product you want, and it will tally these votes at the end in a list and a chart.
+
+It should still operate even if the browser is closed by mistake as it will store the votes in localStorage.
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 'use strict';
 
+// Start at 0, and stop at 25 clicks/choices.
 let totalClicks = 0;
 let maxClicks = 25;
 
-
+// Object constructor
 function ImageObject(name, filePath) {
     this.name = name;
     this.filePath = filePath;
@@ -12,11 +13,13 @@ function ImageObject(name, filePath) {
     this.clicks = 0;
     // this.clickTimes();
 }
+// Array and starting values for the equivalency checkers.
 ImageObject.all = [];
 ImageObject.left = null;
 ImageObject.middle = null;
 ImageObject.right = null;
 
+// Render function, gets filepath and name.
 ImageObject.prototype.render = function (side) {
     const imgElem = document.getElementById(side + '_column_image');
     imgElem.src = this.filePath;
@@ -24,20 +27,16 @@ ImageObject.prototype.render = function (side) {
     this.timesShown += 1;
 }
 
-
+// This function operates for each click on an image.
 function clickTimes(event) {
     totalClicks += 1;
 
     const targetId = event.target.id;
 
-
-
-
     if (targetId === 'left_column_image') {
 
         ImageObject.left.clicks += 1;
         console.log(ImageObject.left.clicks);
-
     }
     else if (targetId === 'middle_column_image') {
         ImageObject.middle.clicks += 1;
@@ -51,21 +50,16 @@ function clickTimes(event) {
         alert('Please select an image!');
         return;
     }
-
-
-    if (maxClicks === totalClicks) {
+    if (maxClicks === totalClicks) { // Ends the clicking, renders the list and chart.
 
         removeEventListeners();
         renderList();
-        renderChart();        
+        renderChart();
         localStorage.clear();
 
-   
     }
     else {
         chooseImages();
-
-
         renderPics();
         storeClicks();
 
@@ -75,6 +69,7 @@ function clickTimes(event) {
 
 }
 
+// Random number function via array index number.
 function giveMeRandom() {
     const index = Math.floor(Math.random() * ImageObject.all.length);
     return ImageObject.all[index];
@@ -84,29 +79,27 @@ function giveMeRandom() {
 // Choose images also checks for repeats.
 function chooseImages() {
 
+    let oldLeft = ImageObject.left;
+    let oldMiddle = ImageObject.middle;
+    let oldRight = ImageObject.right;
+    
 
-
-
-    const oldLeft = ImageObject.left;
-    const oldMiddle = ImageObject.middle;
-    const oldRight = ImageObject.right;
     do {
         ImageObject.left = giveMeRandom();
-    } while (ImageObject.left === oldLeft || ImageObject.left === oldMiddle || ImageObject.left === oldRight)
+    } while (ImageObject.left === ImageObject.middle || ImageObject.left === ImageObject.right || ImageObject.left === oldLeft || ImageObject.left === oldMiddle || ImageObject.left === oldRight)
     do {
         ImageObject.middle = giveMeRandom();
-    } while (ImageObject.left === ImageObject.middle || ImageObject.middle === oldLeft || ImageObject.middle === oldMiddle || ImageObject.middle === oldRight);
+    } while (ImageObject.right === ImageObject.middle || ImageObject.left === ImageObject.middle || ImageObject.middle === oldLeft || ImageObject.middle === oldMiddle || ImageObject.middle === oldRight);
     do {
         ImageObject.right = giveMeRandom();
-    } while ((ImageObject.left === ImageObject.right) || (ImageObject.middle === ImageObject.right || ImageObject.right === oldLeft || ImageObject.right === oldMiddle || ImageObject.right === oldRight));
+    } while (ImageObject.left === ImageObject.right || ImageObject.middle === ImageObject.right || ImageObject.right === oldLeft || ImageObject.right === oldMiddle || ImageObject.right === oldRight);
 
     // Thank you to Jordan Watts, Brian Thornburg, and Michael Metcalf
-
 
 }
 
 
-
+// Actually renders the images.
 function renderPics() {
 
     ImageObject.left.render(`left`);
@@ -116,81 +109,75 @@ function renderPics() {
 }
 
 
-
+// Event listener for the all_images ID in the HTML.
 function attachEventListeners() {
     const clickLocation = document.getElementById('all_images');
     clickLocation.addEventListener('click', clickTimes);
 
 }
-
+// Event unlistener for the all_images ID in the HTML.
 function removeEventListeners() {
     const clickLocation = document.getElementById('all_images');
     clickLocation.removeEventListener('click', clickTimes);
 }
 
+// localStorage for clicks. Stringifies it.
 function storeClicks() {
 
-        const clicksJSON = JSON.stringify(ImageObject.all);
-
-        localStorage.setItem('clicks', clicksJSON);
-
-        
+    const clicksJSON = JSON.stringify(ImageObject.all);
+    localStorage.setItem('clicks', clicksJSON);
 
 }
 
+// Summon from local storage and parse it back into an object.
 function recreateClicks() {
-    
+
     const clicksJSONRetrieve = localStorage.getItem('clicks');
-    
+
     if (clicksJSONRetrieve) {
-            let oldObjects = JSON.parse(clicksJSONRetrieve);
-        
+        let oldObjects = JSON.parse(clicksJSONRetrieve);
+
         for (let i = 0; i < oldObjects.length; i += 1) {
             oldObjects = oldObjects[i];
             const reinstantiate = new ImageObject(oldObjects.name, oldObjects.filePath);
             reinstantiate.clicks = oldObjects.clicks;
             reinstantiate.timesShown = oldObjects.timesShown;
 
-
-
         }
-    } else { 
-        populateImages();
-    }
+    } 
 }
 
+// Create the default/no storage image objects.
 function populateImages() {
 
-
-        new ImageObject('bag', 'assets/bag.jpg');
-        new ImageObject('banana', 'assets/banana.jpg');
-        new ImageObject('bathroom', 'assets/bathroom.jpg');
-        new ImageObject('boots', 'assets/boots.jpg');
-        new ImageObject('breakfast', 'assets/breakfast.jpg');
-        new ImageObject('bubblegum', 'assets/bubblegum.jpg');
-        new ImageObject('chair', 'assets/chair.jpg');
-        new ImageObject('cthulhu', 'assets/cthulhu.jpg');
-        new ImageObject('dog-duck', 'assets/dog-duck.jpg');
-        new ImageObject('dragon', 'assets/dragon.jpg');
-        new ImageObject('pen', 'assets/pen.jpg');
-        new ImageObject('pet-sweep', 'assets/pet-sweep.jpg');
-        new ImageObject('scissors', 'assets/scissors.jpg');
-        new ImageObject('shark', 'assets/shark.jpg');
-        new ImageObject('sweep', 'assets/sweep.png');
-        new ImageObject('tauntaun', 'assets/tauntaun.jpg');
-        new ImageObject('unicorn', 'assets/unicorn.jpg');
-        new ImageObject('water-can', 'assets/water-can.jpg');
-        new ImageObject('wine-glass', 'assets/wine-glass.jpg');
-    
-
+    new ImageObject('bag', 'assets/bag.jpg');
+    new ImageObject('banana', 'assets/banana.jpg');
+    new ImageObject('bathroom', 'assets/bathroom.jpg');
+    new ImageObject('boots', 'assets/boots.jpg');
+    new ImageObject('breakfast', 'assets/breakfast.jpg');
+    new ImageObject('bubblegum', 'assets/bubblegum.jpg');
+    new ImageObject('chair', 'assets/chair.jpg');
+    new ImageObject('cthulhu', 'assets/cthulhu.jpg');
+    new ImageObject('dog-duck', 'assets/dog-duck.jpg');
+    new ImageObject('dragon', 'assets/dragon.jpg');
+    new ImageObject('pen', 'assets/pen.jpg');
+    new ImageObject('pet-sweep', 'assets/pet-sweep.jpg');
+    new ImageObject('scissors', 'assets/scissors.jpg');
+    new ImageObject('shark', 'assets/shark.jpg');
+    new ImageObject('sweep', 'assets/sweep.png');
+    new ImageObject('tauntaun', 'assets/tauntaun.jpg');
+    new ImageObject('unicorn', 'assets/unicorn.jpg');
+    new ImageObject('water-can', 'assets/water-can.jpg');
+    new ImageObject('wine-glass', 'assets/wine-glass.jpg');
 
 }
 
+// This function renders the results list in the HTML.
 function renderList() {
 
     const ulElem = document.getElementById('results-list');
 
-    for (let i = 0; i < ImageObject.all.length; i++) {
+    for (let i = 0; i < ImageObject.all.length; i+=1) {
         const image = ImageObject.all[i];
         const liElem = document.createElement('li');
         ulElem.appendChild(liElem);
@@ -200,7 +187,8 @@ function renderList() {
 
 }
 
-function renderChart() { // taken directly from demo
+// Taken directly from demo. Renders the chart.
+function renderChart() { 
 
     const imageNamesArray = [];
     const imageClicksArray = [];
@@ -245,141 +233,10 @@ function renderChart() { // taken directly from demo
 }
 
 
-// start area
+// Starting Functions
 recreateClicks();
 attachEventListeners();
 populateImages();
 chooseImages();
 renderPics();
 
-
-function oldCodeStorageBox() {
-
-// Old code below =========================================
-
-// Need to make it so the function also checks for repeats.
-
-// chooseImages grabs a left, middle, and a right
-// these images are put into the array
-// chooseImages will run again
-// it must check against the previous array
-// if any match, then choose images must run again
-// it will run up against the array again
-// if it doesn't run up against the array, then proceed
-
-//do I use two arrays? Or just use the same array and examine the same stuff inside?
-
-// function repeatCheck() {
-
-//     if (totalClicks > 0) {
-//         let currentArray = []; 
-//         currentArray.push(ImageObject.left.name, ImageObject.middle.name, ImageObject.right.name);
-//         console.log(currentArray);
-
-//         let indexAdjust = 3;
-//         indexAdjust = totalClicks * indexAdjust;
-
-//         for (let i=0; i < 3; i+=1) {
-//             if (currentArray[i] === compareArray[i]) {
-//                 console.log('current array is ' + currentArray[i]);
-//                 console.log('compare array is ' + compareArray[i]);
-//                 console.log('the bad thing happen');
-//             }
-
-
-
-//         }
-
-
-
-//     }
-
-
-// }
-
-// Old code below =========================================
-
-    // ImageObject.left = giveMeRandom();
-
-    // do {ImageObject.left = giveMeRandom(), ImageObject.middle = giveMeRandom(), ImageObject.right = giveMeRandom()}
-    // while (ImageObject.right===ImageObject.middle || ImageObject.left===ImageObject.middle ||  ImageObject.left===ImageObject.right);
-
-
-    // let oldImageLeft = ImageObject.left;
-    // let oldImageMiddle = ImageObject.middle;
-    // let oldImageRight = ImageObject.right;
-
-    // do {
-    //     ImageObject.left = giveMeRandom();
-    // } while (ImageObject.left === oldImageLeft || ImageObject.left === oldImageMiddle || ImageObject.left === oldImageRight);
-
-    // do {
-    //     ImageObject.middle = giveMeRandom();
-    // } while (ImageObject.left === oldImageLeft || ImageObject.left === oldImageMiddle || ImageObject.left === oldImageRight);
-
-    // do {
-    //     ImageObject.right = giveMeRandom();
-    // } while (ImageObject.left === oldImageLeft || ImageObject.left === oldImageMiddle || ImageObject.left === oldImageRight);
-
-
-    // compareArray.push(ImageObject.left.name, ImageObject.middle.name, ImageObject.right.name);
-
-    // repeatCheck();
-    // console.log(compareArray);
-
-// Old code below =========================================
-
-// const leftClick = document.getElementById('left_column_image');
-// const middleClick = document.getElementById('middle_column_image');
-// const rightClick = document.getElementById('right_column_image');
-
-// Old code below =========================================
-    // const leftIndex = safeIndices[0];
-    // const middleIndex = safeIndices[1];
-    // const rightIndex = safeIndices[2];
-
-    // const leftColumnPic = ImageObject.all[leftIndex];
-    // const middleColumnPic = ImageObject.all[middleIndex];
-    // const rightColumnPic = ImageObject.all[rightIndex];
-
-// Old code below =========================================
-
-//getRandomIndices();
-// const getRandomIndices = function () {
-
-    //     let leftIndex = Math.floor(Math.random() * ImageObject.all.length);
-    //     let middleIndex = Math.floor(Math.random() * ImageObject.all.length);
-    //     let rightIndex = Math.floor(Math.random() * ImageObject.all.length);
-
-    //     do {
-    //         middleIndex = Math.floor(Math.random() * ImageObject.all.length);
-    //     }
-    //     while (leftIndex === middleIndex)
-    //     {
-    //         //console.log('left matched middle');
-    //     }
-
-    //     if (middleIndex === rightIndex) {
-
-    //         do {
-    //             rightIndex = Math.floor(Math.random() * ImageObject.all.length);
-    //         }
-    //         while (middleIndex === rightIndex)
-
-    //         //console.log('left matched right');
-    //     }
-
-    //     if (leftIndex === rightIndex) {
-
-    //         do {
-    //             leftIndex = Math.floor(Math.random() * ImageObject.all.length);
-    //         }
-    //         while (leftIndex === rightIndex)
-
-    //     }
-
-
-    //     // console.log(leftIndex, middleIndex, rightIndex);
-    //     return [leftIndex, middleIndex, rightIndex];
-    // }
-    }
